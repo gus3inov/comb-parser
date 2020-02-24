99d21a334e69959aa21bedf3455f4cc097389ae3
Muslim Guseinov <gusejnov1999@list.ru>
2020-02-24 17:03:14 +0300

    adding type alias for result parser, map result parser

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,10 @@ struct Element {
     children: Vec<Element>,
 }
 
+type ParseResult<'a, Output> = Result<(&'a str, Output), &'a str>;
+
 #[allow(dead_code)]
-fn build_parser(expected: &'static str) -> impl Fn(&str) -> Result<(&str, String), &str> {
+fn build_parser(expected: &'static str) -> impl Fn(&str) -> ParseResult<String> {
     move |input| match input.get(0..expected.len()) {
         Some(next) if next == expected => Ok((&input[expected.len()..], next.to_string())),
         _ => Err(input),
@@ -30,7 +32,7 @@ fn literal_parser() {
     assert_eq!(Err("Hello, Mike!"), parse_joe("Hello, Mike!"));
 }
 
-fn parse_identifier(input: &str) -> Result<(&str, String), &str> {
+fn parse_identifier(input: &str) -> ParseResult<String> {
     let mut matched = String::new();
     let mut chars = input.chars();
 
@@ -70,10 +72,10 @@ fn identifier_parser() {
     );
 }
 
-fn pair<P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Fn(&str) -> Result<(&str, (R1, R2)), &str>
+fn pair<P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Fn(&str) -> ParseResult<(R1, R2)>
 where
-    P1: Fn(&str) -> Result<(&str, R1), &str>,
-    P2: Fn(&str) -> Result<(&str, R2), &str>,
+    P1: Fn(&str) -> ParseResult<R1>,
+    P2: Fn(&str) -> ParseResult<R2>,
 {
     move |input| match parser1(input) {
         Ok((next_input, result1)) => match parser2(next_input) {
@@ -97,4 +99,12 @@ fn pair_combinator() {
     assert_eq!(Err(" oops />"), tag_opener(" oops />"));
 
     assert_eq!(Err("!oops"), tag_opener("<!oops"));
+}
+
+fn map<P, F, A, B>(parser: P, map_fn: F) -> impl Fn(&str) -> ParseResult<B>
+where
+    P: Fn(&str) -> ParseResult<A>,
+    F: Fn(A) -> B,
+{
+    move |input| parser(input).map(|(next_input, result)| (next_input, map_fn(result)))
 }
