3e422ce03ca2fe532338e998d83a7dfab958a4e7
Muslim Guseinov <gusejnov1999@list.ru>
2020-03-09 21:31:42 +0300

    update parsing parent element, adding tests

--- src/lib.rs
+++ src/lib.rs
@@ -413,7 +413,7 @@ where
 }
 
 fn element<'a>() -> impl Parser<'a, Element> {
-    either(single_element(), open_element())
+    whitespace_wrap(either(single_element(), parent_element()))
 }
 
 fn close_element<'a>(expected_name: String) -> impl Parser<'a, String> {
@@ -424,6 +424,62 @@ fn close_element<'a>(expected_name: String) -> impl Parser<'a, String> {
     .pred(move |name| name == &expected_name)
 }
 
-// fn parent_element<'a>() -> impl Parser<'a, Element> {
-//     pair(open_element(), left(zero_or_more(element()), close_element(...oops)))
-// }
+fn parent_element<'a>() -> impl Parser<'a, Element> {
+    open_element().and_then(|el| {
+        left(zero_or_more(element()), close_element(el.name.clone())).map(move |children| {
+            let mut el = el.clone();
+            el.children = children;
+            el
+        })
+    })
+}
+
+fn whitespace_wrap<'a, P, A>(parser: P) -> impl Parser<'a, A>
+where
+    P: Parser<'a, A>,
+{
+    right(space0(), left(parser, space0()))
+}
+
+#[test]
+fn xml_parser() {
+    let doc = r#"
+    <top label="Top">
+        <semi-bottom label="Bottom"/>
+        <middle>
+            <bottom label="Another bottom"/>
+        </middle>
+    </top>"#;
+
+    let parsed_doc = Element {
+        name: "top".to_string(),
+        attributes: vec![("label".to_string(), "Top".to_string())],
+        children: vec![
+            Element {
+                name: "semi-bottom".to_string(),
+                attributes: vec![("label".to_string(), "Bottom".to_string())],
+                children: vec![],
+            },
+            Element {
+                name: "middle".to_string(),
+                attributes: vec![],
+                children: vec![Element {
+                    name: "bottom".to_string(),
+                    attributes: vec![("label".to_string(), "Another bottom".to_string())],
+                    children: vec![],
+                }],
+            },
+        ],
+    };
+
+    assert_eq!(Ok(("", parsed_doc)), element().parse(doc))
+}
+
+#[test]
+fn mismatched_closing_tag() {
+    let doc = r#"
+        <top>
+            <bottom/>
+        </middle>"#;
+    assert_eq!(Err("</middle>"), element().parse(doc));
+}
